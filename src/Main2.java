539e6fe90f31014971835887170bf3b262094492
harukasato0215 <popo02150125@gmail.com>
2024-01-09 16:39:09 +0900

    commit

---
+++ src/Main2.java
@@ -0,0 +1,13 @@
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		int age;
+		age = 30;
+		System.out.println(age);
+		//↑取得
+		
+
+	}
+
+}
---
+++ src/Main5.java
@@ -0,0 +1,13 @@
+
+public class Main5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int height = 3;
+		int width = 5;
+		int area = height * width ;
+		System.out.println("縦幅"+ height + "横幅"+ width +"の長方形の面積は、"+ area);
+
+	}
+
+}
